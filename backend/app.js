3a34edd8bc918072d9d27d154cabaa5b1ba6adb7
Sereina <amandamoulin731@gmail.com>
2021-09-15 23:00:28 +0200

    backend progress -> login and signup

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const dotenv = require ('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+const mysql = require('mysql');
+
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+//CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+//Security & data
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cors());
+
+
+app.use('api/auth', userRoutes);
+
+module.exports = app;
