4118958fb4fab1f031bb6c6907ec1bd4ce3affcc
Sereina <amandamoulin731@gmail.com>
2021-10-15 21:08:55 +0200

    set s3/multers3

--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -1,4 +1,6 @@
+var aws = require('aws-sdk')
 const multer = require('multer');
+var multerS3 = require('multer-s3')
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -6,30 +8,40 @@ const MIME_TYPES = {
   'image/png': 'png'
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
+var s3 = new aws.S3({
+  accessKeyId: process.env.MINIO_KEY,
+  secretAccessKey: process.env.MINIO_SECRET,
+  endpoint: 'http://127.0.0.1:9000',
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4'
+});
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, 'images');
+//   },
+//   filename: (req, file, callback) => {
+//     const name = file.originalname.split(' ').join('_');
+//     const extension = MIME_TYPES[file.mimetype];
+//     callback(null, name + Date.now() + '.' + extension);
+//   }
+// });
+
+
+var upload = multer({ 
+  storage: multerS3({
+    s3: s3,
+    bucket: 'media',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString() + '.' + MIME_TYPES[file.mimetype])
+    }
+  }) 
 });
 
 
-// var upload = multer({
-//   storage: multerS3({
-//     s3: s3,
-//     bucket: 'media',
-//     metadata: function (req, file, cb) {
-//       cb(null, {fieldName: file.fieldname});
-//     },
-//     key: function (req, file, cb) {
-//       cb(null, Date.now().toString())
-//     }
-//   })
-// })
-
-
-module.exports = multer({storage: storage}).single('content');
+// module.exports = multer({storage: storage}).single('content');
+module.exports = upload;
