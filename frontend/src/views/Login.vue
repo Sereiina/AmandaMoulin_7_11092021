d6ed870ffe8c4093bead22672e4cac3e0594715d
Sereina <amandamoulin731@gmail.com>
2021-10-21 19:42:56 +0200

    Login avec session utilisant le token

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,12 +1,33 @@
 <script>
 import LoginSignupNav from '../components/LoginSignupNav.vue'
+import axios from "axios";
+import router from "../router"
 
 export default {
     name: 'Signup',
     components: {
         LoginSignupNav
+    },
+
+    data() {
+        return {
+            inputEmail: "",
+            inputPassword: ""
+        }
+    },
+    methods: {
+
+        async login() {
+
+            const axiosRes = await axios.post("http://localhost:3000/api/auth/login", {
+                email: this.inputEmail,
+                password: this.inputPassword
+            })
+            sessionStorage.setItem("token", axiosRes.data.token );
+            await router.push({ path: '/accueil'});
+        }
     }
-}  
+}
 </script>
 
 
@@ -14,21 +35,21 @@ export default {
     <section>
         <LoginSignupNav />
 
-        <form action="">
+                <form @submit.prevent="login">
             <h1 class="font-center">Connexion </h1>
             
             <div class="wrap-form">
                 <label  for=""></label>
-                <input class="form-margin font-bold" type="email" placeholder="Adresse email">
+                <input class="form-margin font-bold" type="email" placeholder="Adresse email" v-model="inputEmail">
             </div>
 
             <div class="wrap-form">
                 <label class="" for=""></label>
-                <input class="form-margin font-bold" type="password" placeholder="Mot de passe">
+                <input class="form-margin font-bold" type="password" placeholder="Mot de passe" v-model="inputPassword">
             </div>
 
             <div class="wrap-form">
-                <input class="form-margin" type="button" value="Se connecter">
+                <input class="form-margin" type="submit" value="Se connecter">
             </div>
 
         </form>
