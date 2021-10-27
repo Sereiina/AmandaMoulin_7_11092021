9b51c0a1f60343203c57b1db300be4cd984b51dd
Sereina <amandamoulin731@gmail.com>
2021-10-27 19:16:56 +0200

    fonctionnalité supprimant un commentaire

--- frontend/src/components/Comment.vue
+++ frontend/src/components/Comment.vue
@@ -1,14 +1,21 @@
 <script>
+import CommentDelete from '../components/CommentDelete.vue';
 export default {
   name: "Comment",
   props: ["comment"],
+
+  components: {
+    CommentDelete,
+  }
 };
 </script>
 
 
 <template>
   <div>
+
     <p>{{ this.comment.content }}</p>
+    <CommentDelete :postId="this.comment.postId" :commentId="this.comment.commentId" />
   </div>
 </template>
 
